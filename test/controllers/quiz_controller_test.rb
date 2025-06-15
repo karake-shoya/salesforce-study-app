c1204b3a1b45c9ad8410eafa706de5ad0d71b5a3
naiawa <karake.shoya@gmail.com>
2025-06-15 12:43:45 +0900

    fix: テストファイルの修正とテストデータの追加

--- test/controllers/quiz_controller_test.rb
+++ test/controllers/quiz_controller_test.rb
@@ -1,18 +1,22 @@
 require "test_helper"
 
 class QuizControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @question = questions(:one)
+  end
+
   test "should get index" do
     get quiz_index_url
     assert_response :success
   end
 
-  test "should get show" do
-    get quiz_show_url
+  test "should get show with question" do
+    get quiz_url(@question)
     assert_response :success
   end
 
   test "should get result" do
-    get quiz_result_url
+    get result_quiz_index_url
     assert_response :success
   end
 end
--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -1,7 +1,39 @@
 require "test_helper"
 
 class QuestionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should be valid with valid attributes" do
+    question = Question.new(
+      question: "テスト問題",
+      option_a: "選択肢A",
+      option_b: "選択肢B",
+      correct_answers: "A",
+      category: "テストカテゴリー"
+    )
+    assert question.valid?
+  end
+
+  test "should require question" do
+    question = Question.new
+    assert_not question.valid?
+    assert_includes question.errors[:question], "can't be blank"
+  end
+
+  test "should return correct answer array" do
+    question = questions(:two)
+    assert_equal [ "A", "C" ], question.correct_answer_array
+  end
+
+  test "should check if answer is correct" do
+    question = questions(:one)
+    assert question.is_correct?([ "A" ])
+    assert_not question.is_correct?([ "B" ])
+  end
+
+  test "should check multiple correct answers" do
+    question = questions(:two)
+    assert question.is_correct?([ "A", "C" ])
+    assert question.is_correct?([ "C", "A" ])
+    assert_not question.is_correct?([ "A" ])
+    assert_not question.is_correct?([ "A", "B" ])
+  end
 end
